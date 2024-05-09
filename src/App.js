9255623cc2dbe4473438dbc599db22ed0d069534
Priyanka <133139652+Priyanka6p@users.noreply.github.com>
2024-05-09 19:30:34 +0530

    New Formik Form add

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import StopCounter from './components/StopCounter/StopCounter';
 import PostAPI from './components/Post/PostAPI';
@@ -7,10 +7,10 @@ import Debouncing from './components/Debouncing';
 import { NavBar } from './components/NavBar';
 import Child from './components/Parent Child/Child';
 import NewsList from './components/News/NewsList';
-import dayjs from 'dayjs';
 import Dayjs from './components/Dayjs';
 
 function App() {
+  console.log("ENV File data: ", process.env.REACT_APP_SECRET_NAME);
   return (
     <>
       <BrowserRouter>
